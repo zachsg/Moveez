a5b1b2984f47af3a6302b6c0fd15f185d4b18b44
zachsg <zsgott@gmail.com>
2017-09-10 16:55:16 -0700

    Remove unnecessary imports after changing from AsyncTask to AsyncTaskLoaders

--- app/src/main/java/com/example/android/moveez/MovieDetailsActivity.java
+++ app/src/main/java/com/example/android/moveez/MovieDetailsActivity.java
@@ -1,12 +1,11 @@
 package com.example.android.moveez;
 
 import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Parcelable;
-import android.os.StrictMode;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -17,15 +16,15 @@ import com.example.android.moveez.data.Movie;
 import com.example.android.moveez.utilities.JsonUtils;
 import com.example.android.moveez.utilities.NetworkUtils;
 
-import org.w3c.dom.Text;
-
 import java.net.URL;
-import java.util.List;
 import java.util.Locale;
 
-public class MovieDetailsActivity extends AppCompatActivity {
+public class MovieDetailsActivity
+        extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Movie> {
 
     private static final String LOG_TAG = MovieDetailsActivity.class.getSimpleName();
+
+    private static final int LOADER_ID = 2;
 
     private Integer mMovieID;
 
@@ -61,7 +60,13 @@ public class MovieDetailsActivity extends AppCompatActivity {
             Movie movie = (Movie) bundle.getSerializable("movie");
             try {
                 mMovieID = movie.getId();
-                new MovieDetailsActivity.MovieTask().execute(mMovieID.toString());
+                LoaderManager loaderManager = getSupportLoaderManager();
+                loaderManager.getLoader(LOADER_ID);
+                if (loaderManager == null) {
+                    loaderManager.initLoader(LOADER_ID, null, this);
+                } else {
+                    loaderManager.restartLoader(LOADER_ID, null, this);
+                }
             } catch (NullPointerException npe) {
                 npe.printStackTrace();
             }
@@ -70,53 +75,61 @@ public class MovieDetailsActivity extends AppCompatActivity {
         }
     }
 
-    private class MovieTask extends AsyncTask<String, Void, Movie> {
-
-        @Override
-        protected void onPreExecute() {
-            mDetailsSV.setVisibility(View.INVISIBLE);
-            mLoadingPB.setVisibility(View.VISIBLE);
-            super.onPreExecute();
-        }
-
-        @Override
-        protected Movie doInBackground(String... strings) {
-            // Pass in the ID of the movie we're getting details for
-            URL url = NetworkUtils.buildUrl(mMovieID.toString());
-            try {
-                String moviesString = NetworkUtils.getResponseFromHttpUrl(url);
-                Log.v(LOG_TAG, moviesString);
-                return JsonUtils.getMovieDetails(moviesString);
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
+    @Override
+    public Loader<Movie> onCreateLoader(int id, Bundle args) {
+        return new AsyncTaskLoader<Movie>(this) {
+            @Override
+            protected void onStartLoading() {
+                mDetailsSV.setVisibility(View.INVISIBLE);
+                mLoadingPB.setVisibility(View.VISIBLE);
+                forceLoad();
             }
-        }
 
-        @Override
-        protected void onPostExecute(Movie movie) {
-            mTitleTV.setText(movie.getTitle());
-            mReleaseDateTV.setText(movie.getReleaseDate().substring(0, 4));
-            mRuntimeTV.setText(formatRuntime(movie.getRuntime()));
-            mRatingTV.setText(formatRating(movie.getVoteCount(), movie.getVoteAverage()));
-            mDescriptionTV.setText(movie.getOverview());
+            @Override
+            public Movie loadInBackground() {
+                URL url = NetworkUtils.buildUrl(mMovieID.toString());
+                try {
+                    String moviesString = NetworkUtils.getResponseFromHttpUrl(url);
+                    return JsonUtils.getMovieDetails(moviesString);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Movie> loader, Movie data) {
+        if (data != null) {
+            mTitleTV.setText(data.getTitle());
+            mReleaseDateTV.setText(data.getReleaseDate().substring(0, 4));
+            mRuntimeTV.setText(formatRuntime(data.getRuntime()));
+            mRatingTV.setText(formatRating(data.getVoteCount(), data.getVoteAverage()));
+            mDescriptionTV.setText(data.getOverview());
 
             // Load the movie poster into the ImageView
-            NetworkUtils.loadImage(MovieDetailsActivity.this, mPosterIV, movie);
+            NetworkUtils.loadImage(MovieDetailsActivity.this, mPosterIV, data);
 
             mLoadingPB.setVisibility(View.INVISIBLE);
             mDetailsSV.setVisibility(View.VISIBLE);
+        } else {
+            // TODO: Show error, movie details weren't loaded
         }
+    }
 
-        private String formatRuntime(int runtime) {
-            int hours = runtime / 60;
-            int minutes = runtime - (hours*60);
-            if (hours > 0) return String.format(Locale.ENGLISH, "%dhr, %dmin", hours, minutes);
-            else return String.format(Locale.ENGLISH, "%dmin", minutes);
-        }
+    @Override
+    public void onLoaderReset(Loader<Movie> loader) {
+    }
 
-        private String formatRating(int ratingCount, double ratingAverage) {
-            return String.format(Locale.ENGLISH, "%.1f/10 (%d ratings)", ratingAverage, ratingCount);
-        }
+    private String formatRuntime(int runtime) {
+        int hours = runtime / 60;
+        int minutes = runtime - (hours * 60);
+        if (hours > 0) return String.format(Locale.ENGLISH, "%dhr, %dmin", hours, minutes);
+        else return String.format(Locale.ENGLISH, "%dmin", minutes);
+    }
+
+    private String formatRating(int ratingCount, double ratingAverage) {
+        return String.format(Locale.ENGLISH, "%.1f/10 (%d ratings)", ratingAverage, ratingCount);
     }
 }
